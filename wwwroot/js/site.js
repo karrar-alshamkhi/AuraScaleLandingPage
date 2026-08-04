5791376dc5bfd493d5a58c15083acb66eee1580c
كرار قصي عبدالجليل <karraralhesnawi94@gmail.com>
2026-08-04 11:57:32 +0300

    Add files via upload
    اضافة الملفات الرئيسية

---
+++ wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Mobile navigation toggle
+(function () {
+    var toggle = document.querySelector('.site-nav__toggle');
+    var nav = document.getElementById('site-nav');
+    if (toggle && nav) {
+        toggle.addEventListener('click', function () {
+            var isOpen = nav.classList.toggle('is-open');
+            toggle.setAttribute('aria-expanded', isOpen ? 'true' : 'false');
+        });
+    }
+}());
