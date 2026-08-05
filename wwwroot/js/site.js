2c3365e87989f9ade3ee336d05df61e1c294fd02
كرار قصي عبدالجليل <karraralhesnawi94@gmail.com>
2026-08-05 11:32:55 +0300

    Implement image comparison slider feature
    Added image comparison slider functionality to site.js.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
 // Mobile navigation toggle
@@ -9,6 +9,18 @@
         toggle.addEventListener('click', function () {
             var isOpen = nav.classList.toggle('is-open');
             toggle.setAttribute('aria-expanded', isOpen ? 'true' : 'false');
+        });
+    }
+}());
+
+// Image comparison slider
+(function () {
+    var container = document.querySelector('.img-compare');
+    var slider = document.querySelector('.img-compare__slider');
+
+    if (container && slider) {
+        slider.addEventListener('input', function (e) {
+            container.style.setProperty('--position', e.target.value + '%');
         });
     }
 }());
